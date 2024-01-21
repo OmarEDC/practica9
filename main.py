b45d03c2c0ba315d58fc7f0e8558afc786875fc7
Omar Ulises <79721849+OmarEDC@users.noreply.github.com>
2024-01-21 03:27:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+def hello():
+    return '¡Hola, esta es una aplicación Flask desplegada en la nube!'
